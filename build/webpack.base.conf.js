3aabd5196a627ccfff93e9aafdb0032ed49a7317
neveleneves <max.nikonov.work@gmail.com>
2020-10-15 01:37:08 +0300

    config base build dev | config upload img and another assets files

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -39,6 +39,7 @@ module.exports = {
         {
             test: /\.css$/,
             use: [
+                //'style-loader',
                 MiniCssExtractPlugin.loader,
                 {
                     loader: "css-loader",
