70abd2a5c89569fec5642fd4f6abb601a6da67f3
neveleneves <max.nikonov.work@gmail.com>
2020-10-28 21:19:48 +0300

    100% done layout intro page for 1920px

--- src/js/assets.js
+++ src/js/assets.js
@@ -1,33 +1,33 @@
-//Burger-menu control
-(function(){
-    const burgerMenu = document.querySelector('.burger');
-    const menu = document.querySelector('.header-nav');
-    const burgerMenuClose = document.querySelector('.header-burger-close');
+// //Burger-menu control
+// (function(){
+//     const burgerMenu = document.querySelector('.burger');
+//     const menu = document.querySelector('.header-nav');
+//     const burgerMenuClose = document.querySelector('.header-burger-close');
 
-    const searchBlockState = document.getElementById('search-state');
-    const searchBlock = document.querySelector('.search-form-adaptive');
-    const searchButton = document.querySelector('.search-button');
+//     const searchBlockState = document.getElementById('search-state');
+//     const searchBlock = document.querySelector('.search-form-adaptive');
+//     const searchButton = document.querySelector('.search-button');
 
-    burgerMenu.addEventListener('click', () => {
-        if(window.getComputedStyle(searchBlockState, null).display != 'none') searchBlock.classList.remove('search-form-adaptive-active');
-        menu.classList.add('header-nav-active');
-    });
-    burgerMenuClose.addEventListener('click', () => {
-        menu.classList.remove('header-nav-active');
-    });
-}());
+//     burgerMenu.addEventListener('click', () => {
+//         if(window.getComputedStyle(searchBlockState, null).display != 'none') searchBlock.classList.remove('search-form-adaptive-active');
+//         menu.classList.add('header-nav-active');
+//     });
+//     burgerMenuClose.addEventListener('click', () => {
+//         menu.classList.remove('header-nav-active');
+//     });
+// }());
 
-//Search-button control
-(function(){
-    const searchButton = document.querySelector('.search-button');
-    const searchBlock = document.querySelector('.search-form-adaptive');
-    const searchBlockState = document.getElementById('search-state');
+// //Search-button control
+// (function(){
+//     const searchButton = document.querySelector('.search-button');
+//     const searchBlock = document.querySelector('.search-form-adaptive');
+//     const searchBlockState = document.getElementById('search-state');
 
-    searchButton.addEventListener('click', () => {
-        if(window.getComputedStyle(searchBlockState, null).display == 'none') {
-            searchBlock.classList.add('search-form-adaptive-active');
-        } else if (window.getComputedStyle(searchBlockState, null).display == 'block') {
-            searchBlock.classList.remove('search-form-adaptive-active');
-        }
-    });
-}());
+//     searchButton.addEventListener('click', () => {
+//         if(window.getComputedStyle(searchBlockState, null).display == 'none') {
+//             searchBlock.classList.add('search-form-adaptive-active');
+//         } else if (window.getComputedStyle(searchBlockState, null).display == 'block') {
+//             searchBlock.classList.remove('search-form-adaptive-active');
+//         }
+//     });
+// }());
