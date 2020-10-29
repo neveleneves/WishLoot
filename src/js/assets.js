f4a80644ec5a3d638b95bf93c12b25a969b81bd0
neveleneves <max.nikonov.work@gmail.com>
2020-10-29 18:47:44 +0300

    + adapive sctipts for full version

--- src/js/assets.js
+++ src/js/assets.js
@@ -1,33 +1,38 @@
-// //Burger-menu control
-// (function(){
-//     const burgerMenu = document.querySelector('.burger');
-//     const menu = document.querySelector('.header-nav');
-//     const burgerMenuClose = document.querySelector('.header-burger-close');
+//Burger-menu control
+(function(){
 
-//     const searchBlockState = document.getElementById('search-state');
-//     const searchBlock = document.querySelector('.search-form-adaptive');
-//     const searchButton = document.querySelector('.search-button');
+    if(window.location.href == 'http://localhost:8082/content.html'){
+        const burgerMenu = document.querySelector('.burger');
+    const menu = document.querySelector('.header-nav');
+    const burgerMenuClose = document.querySelector('.header-burger-close');
 
-//     burgerMenu.addEventListener('click', () => {
-//         if(window.getComputedStyle(searchBlockState, null).display != 'none') searchBlock.classList.remove('search-form-adaptive-active');
-//         menu.classList.add('header-nav-active');
-//     });
-//     burgerMenuClose.addEventListener('click', () => {
-//         menu.classList.remove('header-nav-active');
-//     });
-// }());
+    const searchBlockState = document.getElementById('search-state');
+    const searchBlock = document.querySelector('.search-form-adaptive');
+    const searchButton = document.querySelector('.search-button');
 
-// //Search-button control
-// (function(){
-//     const searchButton = document.querySelector('.search-button');
-//     const searchBlock = document.querySelector('.search-form-adaptive');
-//     const searchBlockState = document.getElementById('search-state');
+    burgerMenu.addEventListener('click', () => {
+        if(window.getComputedStyle(searchBlockState, null).display != 'none') searchBlock.classList.remove('search-form-adaptive-active');
+        menu.classList.add('header-nav-active');
+    });
+    burgerMenuClose.addEventListener('click', () => {
+        menu.classList.remove('header-nav-active');
+    });
+    }
+}());
 
-//     searchButton.addEventListener('click', () => {
-//         if(window.getComputedStyle(searchBlockState, null).display == 'none') {
-//             searchBlock.classList.add('search-form-adaptive-active');
-//         } else if (window.getComputedStyle(searchBlockState, null).display == 'block') {
-//             searchBlock.classList.remove('search-form-adaptive-active');
-//         }
-//     });
-// }());
+//Search-button control
+(function(){
+    if(window.location.href == 'http://localhost:8082/content.html'){
+        const searchButton = document.querySelector('.search-button');
+    const searchBlock = document.querySelector('.search-form-adaptive');
+    const searchBlockState = document.getElementById('search-state');
+
+    searchButton.addEventListener('click', () => {
+        if(window.getComputedStyle(searchBlockState, null).display == 'none') {
+            searchBlock.classList.add('search-form-adaptive-active');
+        } else if (window.getComputedStyle(searchBlockState, null).display == 'block') {
+            searchBlock.classList.remove('search-form-adaptive-active');
+        }
+    });
+    }
+}());
