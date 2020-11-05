72caf3599109e7cb9a06dbc0791e5805c7dbeb84
neveleneves <max.nikonov.work@gmail.com>
2020-11-05 23:45:22 +0300

    small adjustments

--- src/js/controller.js
+++
@@ -1,7 +0,0 @@
-const StockXAPI = require('stockx-api');
-const stockX = new StockXAPI();
-stockX.newSearchProducts('yeezy', {
-    limit: 5
-})
-.then(products => console.log(products))
-.catch(err => console.log(`Error searching: ${err.message}`));
